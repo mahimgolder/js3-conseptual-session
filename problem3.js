0fc3cb4890c15f35a4c274b05113d831050513e7
mahimgolder <mahimgolder@gmail.com>
2024-10-10 09:44:38 -0600

    wrote two conditions to check the parameters of the function

--- problem3.js
+++ problem3.js
@@ -1,13 +1,24 @@
 // apnar mashik khoroj , ghor-vara = 5000 , khabar-khoroj = 3000, hat-khoroj = 2000, mash sese apnar savings koto hobe? amnar ayer upoe vitti kore ; 
 function calculateSavings(income){
+
+    // ****function r paramiter check korbe :
+    if(typeof income !== 'number' ){
+        return "Please enter a number....!"
+    }
+
     const houseRent = 5000;
     const mealCost = 3000; 
     const personalExpenses = 2000;
+    const totalExpenses = houseRent + mealCost + personalExpenses;//total income tk
 
-    const totalExpenses = houseRent + meltCost + personalExpenses;
+
+//jodi income r theke totalExpenses  beshi hoy : 
+    if(totalExpenses > income){
+        return 'Earn more money..!';
+    }
 
     const savings = income - totalExpenses;
     return savings;
 }
-const result = calculateSavings(20000);
+const result = calculateSavings(5000);
 console.log(result);
