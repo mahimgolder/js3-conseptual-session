0036381bec5b9d94e3dcb290fc652818e547e6ff
mahimgolder <mahimgolder@gmail.com>
2024-10-10 11:13:44 -0600

    created this solveing plan

---
+++ problem5.js
@@ -0,0 +1,17 @@
+/*
+biye-bari bor-jatri jaoyar poribohon:
+bus = 50;
+micro = 15;
+rickshaw cast tk = 20;
+
+
+******solveing plan :
+
+total borjatri jabbe : 234 jon;
+----------
+bus (4) = 200 jon,----remaining = 34 jon;
+micro (2) = 30 jon,---remaining = 4 jon;
+remaining 4 jon ricksha te jabe .
+rickshaw (4 jon) = vara hobe -- (20 * 4)=80tk
+
+*/
