d46d9ee83247149644d6ed0b48987e649b81bd69
mahimgolder <mahimgolder@gmail.com>
2024-10-10 12:45:41 -0600

    add some if condition

--- job.js
+++ job.js
@@ -10,6 +10,12 @@ const person2 = {
     hsc: 32,
     isFFfamily: false,
 }
+const person3 = {
+    name: "maruf mia",
+    honors: 20,
+    hsc: 20,
+    isFFfamily: true,
+}
 
 function jobSelection (info){
     if(typeof info !== "object"){
@@ -21,14 +27,31 @@ function jobSelection (info){
     if(info.isFFfamily ){
         totalMarks =totalMarks + (totalMarks * 0.2)
     }
+    console.log(totalMarks)
 
-    if(totalMarks >= 80){
-        return "you got the job";
+    if (info.isFFfamily && totalMarks >= 80){
+        return "Aho vatija Aho.....!"
     }
-    else{
-        return "you failed";
+    else if(info.isFFfamily && totalMarks < 80){
+        return "Bad luck vatija ...Try nest time!"
     }
+    else if(!info.isFFfamily && totalMarks >=80){
+        return "you got the job."
+    }
+    else if(!info.isFFfamily && totalMarks < 80) {
+        console.log( typeof info.isFFfamily);
+        return "rajakar rajakar ....!"
+    }
+
+
+    // if(totalMarks >= 80){
+    //     return "you got the job";
+    // }
+    // else{        console.log( typeof info.isFFfamily);
+
+    //     return "you failed";
+    // }
     
 }
 
-console.log(jobSelection(person1));
+console.log(jobSelection(person3));
