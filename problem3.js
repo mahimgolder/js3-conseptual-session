e704c1f7f8a089ebbaddae06084e5bdb72600667
mahimgolder <mahimgolder@gmail.com>
2024-10-10 09:11:03 -0600

    Diclares an function witch calculates monthly savings  ,

---
+++ problem3.js
@@ -0,0 +1,13 @@
+// apnar mashik khoroj , ghor-vara = 5000 , khabar-khoroj = 3000, hat-khoroj = 2000, mash sese apnar savings koto hobe? amnar ayer upoe vitti kore ; 
+function calculateSavings(income){
+    const houseRent = 5000;
+    const mealCost = 3000; 
+    const personalExpenses = 2000;
+
+    const totalExpenses = houseRent + meltCost + personalExpenses;
+
+    const savings = income - totalExpenses;
+    return savings;
+}
+const result = calculateSavings(20000);
+console.log(result);
